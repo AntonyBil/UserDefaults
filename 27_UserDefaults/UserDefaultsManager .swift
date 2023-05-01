a5b11a18501144abc9ea844f9d4334bccd68f902
AntonyBil <beletskiy.ant@gmail.com>
2023-05-01 20:58:54 +0300

    Add variant to use UserDefaults with struct and singelton

---
+++ 27_UserDefaults/UserDefaultsManager .swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsManager.swift
+//  27_UserDefaults
+//
+//  Created by apple on 01.05.2023.
+//
+
+import Foundation
+
+//We also can use struct for UserDefaults
+struct UserDefaultsManeger {
+    static let  shared = UserDefaultsManeger()
+    private init () {}
+    
+    //Here we can use the seme thing like in extension
+    
+}
